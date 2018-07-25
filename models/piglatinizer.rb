573761cd484a218fd21c82c478b72336680ddf00
Christiana Vandermale <christianavdm@gmail.com>
2018-07-25 22:47:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,11 @@ class PigLatinizer
       @text = text.downcase
   end
 
-  def piglatinize 
+  def piglatinize
     words = text.split(" ")
+    if words.count > 1 
+      words.each
   end
+
+  def 
 end
