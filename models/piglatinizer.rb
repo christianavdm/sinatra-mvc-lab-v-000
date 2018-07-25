c5cd351d3d400ce837c862cf436b0c94d9a722d7
Christiana Vandermale <christianavdm@gmail.com>
2018-07-25 23:05:00 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,7 @@ class PigLatinizer
   def piglatinize(word)
     first_letter = word.split('').first
     binding.pry
-    if vowel?(first_letter)
+    if     letter.downcase == "a" || self.downcase == "e" || self.downcase == "i" || self.downcase == "o" || self.downcase == "u"
       word << "way"
     else
   #      put consonants on end of word and add "ay"
